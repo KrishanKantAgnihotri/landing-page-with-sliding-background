f290c2d4a0799b81b25957bb39dcbb1a49c02867
Krishan Kant Agnihotri <60218702+KRISHANKANTAGNIHOTRI@users.noreply.github.com>
2021-07-18 19:27:33 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,20 @@
+// alert();
+var slide = document.getElementById("slide");
+var images = new Array(
+    "images/1.jpeg",
+    "images/2.jpg",
+    "images/3.jpg",
+    "images/4.jpg",
+    "images/5.jpg"
+);
+var len = images.length;
+var i = 1;
+function slider() {
+
+    if (i > len - 1) {
+        i = 0;
+    }
+    slide.src = images[i];
+    i++;
+    setTimeout("slider()", 3000);
+}
